d3d57415614de10b5208e18e0304c6a19614dcc2
Howard <2548795196@qq.com>
2024-04-19 09:34:31 +0800

    Initial Commit

---
+++ MacToolKit/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  MacToolKit
+//
+//  Created by Howard-Zjun on 2024/04/19.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
